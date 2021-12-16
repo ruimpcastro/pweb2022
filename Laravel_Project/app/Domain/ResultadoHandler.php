5de21d2e2ede9c028c7504f5a25e8c7b80fd216e
Rui Castro <rui_castro@live.com.pt>
2021-12-16 19:21:34 -0100

    ResultadoHandler.php

---
+++ Laravel_Project/app/Domain/ResultadoHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+class ResultadoHandler
+{
+    public function gerarPauta()
+    {
+        //Pega no ficheiro csv -> converte para json
+        //Criar pauta
+        //Associar valores do novo json para os valores da pauta
+        //guardar pauta
+    }
+}
