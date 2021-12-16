338db9cd6ae2621454f64df29c8c1ab95f35a0c3
Daniel <20172315@aluno.uac.pt>
2021-12-15 23:40:42 -0100

    API

--- Laravel_Project/routes/api.php
+++ Laravel_Project/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CursoController;
+use App\Http\Resources\CursoResource;
+use App\Models\Curso;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+
 });
+
+Route::get("/cursos/{id}", [CursoController::class,"show"]);
+
+
