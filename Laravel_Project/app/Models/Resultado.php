1e350c17dd2c4d097b63d9c86d903a471b7469ee
Rui Castro <rui_castro@live.com.pt>
2021-12-18 02:00:43 -0100

    Added new mock data

--- Laravel_Project/app/Models/Resultado.php
+++ Laravel_Project/app/Models/Resultado.php
@@ -9,8 +9,6 @@ class Resultado extends Model
 {
     use HasFactory;
 
-
-
     public function pauta()
     {
         return $this->belongsTo(Pauta::class);
@@ -24,11 +22,58 @@ class Resultado extends Model
     {
         $r = new Resultado();
         $r->resultado = "16.5";
+        $a = Aluno::find(1);
+        $p = Pauta::find(1);
+        $r->pauta()->associate($p);
+        $r->save();
+        $r->aluno()->attach($a);
+
+        $r = new Resultado();
+        $r->resultado = "6.66";
         $a = Aluno::find(2);
+        $r->pauta()->associate($p);
+        $r->save();
+        $r->aluno()->attach($a);
+
+        $r = new Resultado();
+        $r->resultado = "9.5";
+        $a = Aluno::find(3);
+        $p = Pauta::find(1);
+        $r->pauta()->associate($p);
+        $r->save();
+        $r->aluno()->attach($a);
+
+        $r = new Resultado();
+        $r->resultado = "F";
+        $a = Aluno::find(4);
+        $p = Pauta::find(1);
+        $r->pauta()->associate($p);
+        $r->save();
+        $r->aluno()->attach($a);
+
+        $r = new Resultado();
+        $r->resultado = "F";
+        $a = Aluno::find(5);
         $p = Pauta::find(2);
         $r->pauta()->associate($p);
         $r->save();
         $r->aluno()->attach($a);
+
+        $r = new Resultado();
+        $r->resultado = "12.1";
+        $a = Aluno::find(6);
+        $r->pauta()->associate($p);
+        $r->save();
+        $r->aluno()->attach($a);
+
+        $r = new Resultado();
+        $r->resultado = "15";
+        $a = Aluno::find(7);
+        $p = Pauta::find(1);
+        $r->pauta()->associate($p);
+        $r->save();
+        $r->aluno()->attach($a);
+
 
         /*
         $r = new Resultado();
