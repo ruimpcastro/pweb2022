951fa81b2c5371364db17e45d653ad29732ebaa4
Rui Castro <rui_castro@live.com.pt>
2022-01-18 15:14:55 -0100

    Started login screen

---
+++ Laravel_Project/resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Gestão de cursos 🔥</title>
+</head>
+
+<body style="height: 100vh; width: 100vw">
+    <div class="card">
+        <div class="card-body">
+            <h3 class="d-flex justify-content-center">Gestão de cursos 🔥</h3>
+            <div>
+                <p><b>Username</b></p>
+                <input type="text" class="form-control" placeholder="Insira o seu número de utilizador" aria-label="Username" aria-describedby="basic-addon1">
+                <p><b>Password</b></p>
+                <input type="password" class="form-control" placeholder="Insira a sua password" aria-label="Username" aria-describedby="basic-addon1">
+                <button type="button" class="btn btn-primary">Login</button>
+                <button type="button" class="btn btn-primary">Voltar para a página principal</button>
+                <p><a href="">Registar novo utilizador</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- Laravel_Project/resources/views/master.blade.php
+++ Laravel_Project/resources/views/master.blade.php
@@ -8,13 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Gestão incrível de cursos 🔥</title>
+    <title>Gestão de cursos 🔥</title>
 </head>
 
 <body style="height: 100vh; width: 100vw">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/"><b>Cursos 🔥</b></a>
+            <a class="navbar-brand" href="/login">Sign in</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
