de6ad6c4dc6d564d960d388d54819625536ab57a
Rui Castro <rui_castro@live.com.pt>
2022-01-18 15:18:07 -0100

    Started login screen

--- Laravel_Project/routes/web.php
+++ Laravel_Project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CursoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
 
 //CURSO
