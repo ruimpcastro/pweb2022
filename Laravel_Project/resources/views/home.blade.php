6bc6a54e03d68214ca01e370e063a5ff651ff40b
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2022-01-20 02:53:47 +0000

    Botao Home

--- Laravel_Project/resources/views/home.blade.php
+++ Laravel_Project/resources/views/home.blade.php
@@ -5,5 +5,7 @@
 @endsection
 
 @section('conteudo')
-    <h3>Conteudo</h3>
+    <h3>Objetivos:</h3>
+    <li>Gestão de cursos/alunos/disciplinas/pautas.</li>
+    <li>A análise do percurso escolar dos estudantes de um dado curso.</li>
 @endsection
