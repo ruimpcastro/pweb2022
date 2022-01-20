cc6b45e1ffc6ce386e363293f60503092054d7d2
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2022-01-20 00:40:34 +0000

    Criar form "adicionar aluno"

--- Laravel_Project/resources/views/criarAluno.blade.php
+++ Laravel_Project/resources/views/criarAluno.blade.php
@@ -8,26 +8,37 @@
     <form method="post" action="/alunos/create">
 
         @csrf
-        <p class="mb-0"><b>Número do Aluno</b></p>
+        <p class="mb-0"><b>Número do aluno</b></p>
+        <input
+            type="number"
+            class="form-control"
+            placeholder="000"
+            aria-label="Username"
+            aria-describedby="numero-aluno"
+            name="cod"
+            value={{ old('cod')}}
+        >
+
+        <p class="mt-4 mb-0"><b>Nome aluno</b></p>
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Informática"
+            aria-label="Username"
+            aria-describedby="name"
+            name="des"
+            value={{ old('des')}}
+        >
+
+        <p class="mt-4 mb-0"><b>Código curso</b></p>
         <input
             type="number"
             class="form-control"
             placeholder="000"
             aria-label="Username"
             aria-describedby="codigo-curso"
-            name="cod"
-            value={{ old('cod')}}
-        >
-
-        <p class="mt-4 mb-0"><b>Designação do curso</b></p>
-        <input
-            type="text"
-            class="form-control"
-            placeholder="Informática"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
             name="des"
-            value={{ old('des')}}
+            value={{ old('cod')}}
         >
 
         <div class="w-100pt-3 d-flex justify-content-center align-items-center mt-2">
