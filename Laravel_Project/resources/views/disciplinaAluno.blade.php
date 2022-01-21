37823b56835d0c3580bf2e84ad8ddb7abdab7f52
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2022-01-21 09:09:51 +0000

    Tabela disciplinaAluno

--- Laravel_Project/resources/views/disciplinaAluno.blade.php
+++ Laravel_Project/resources/views/disciplinaAluno.blade.php
@@ -5,7 +5,18 @@
 @endsection
 @section('conteudo')
     @foreach($disciplina->alunos as $aluno)
-        <p>{{$aluno->numero_aluno}}</p>
-        <p>{{$aluno->nome}}</p>
+
+        <ul style="list-style: none" class="d-flex justify-content-between align-items-center flex-row ps-0 border-bottom mb-3">
+            <div class="d-flex flex-row justify-content-center align-items-center">
+                <div class="me-5">
+                    <li>{{ $aluno->numero_aluno }}</li>
+                </div>
+                <div>
+                    <!--<a href="/aluno/{{$aluno->numero_aluno}}">-->
+                        <li>{{ $aluno->nome }}</li>
+                    </a>
+                </div>
+            </div>
+        </ul>
     @endforeach
 @endsection
