4986a504c6c1dc2749b1dbf177119b52d5bc657b
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2022-01-21 00:34:57 +0000

    eliminar codigo repetido

--- Laravel_Project/resources/views/disciplinaPauta.blade.php
+++ Laravel_Project/resources/views/disciplinaPauta.blade.php
@@ -14,14 +14,14 @@
         <p>
             @if($pauta->dirty == 1)
                 <button type="button" class="btn btn-secondary" disabled>Pauta publicada</button>
-                <button type="button" method="GET" action="/pauta/{{$disciplina->codigo}}" class="btn btn-primary">Visualizar pauta</button>
+                <button type="button" method="GET" action="" class="btn btn-primary">Visualizar pauta</button>
             @else
 
                 <!--TODO Adicionar ligação entre dar upload do ficheiro e o guardar -->
 
                 <div class="input-group mb-3">
                     <input type="file" class="form-control" id="inputGroupFile01">
-                    <button type="button" method="POST" action="/disciplina/{{$disciplina->codigo}}/pautas" class="btn btn-primary">Guardar</button>
+                    <button type="button" method="POST" action="" class="btn btn-primary">Guardar</button>
                 </div>
             @endif
         </p>
