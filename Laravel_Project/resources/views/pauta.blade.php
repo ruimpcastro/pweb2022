728c4986005f173994c284ad44042ebc74d1f0b7
Daniel <20172315@aluno.uac.pt>
2022-01-22 02:00:24 -0100

    Acrescentar os textos

--- Laravel_Project/resources/views/pauta.blade.php
+++ Laravel_Project/resources/views/pauta.blade.php
@@ -8,4 +8,25 @@
     {{ $pauta }}
     {{ $pauta->resultado }}
     {{ $disciplina }}
+
+    @if($pauta->dirty == 1)
+
+
+        <!--TODO Ligação para se poder visualizar a pauta publicada -->
+
+        <p>Estado da Pauta: Publicada </p>
+        <p>Resultados: </p>
+
+
+    @else
+
+        <!--TODO Adicionar ligação entre dar upload do ficheiro e o guardar -->
+
+        <div class="input-group mb-3">
+            <input type="file" class="form-control" id="inputGroupFile01">
+            <p> Estado da Pauta: Por Publicar </p>
+        </div>
+    @endif
+
+
 @endsection
