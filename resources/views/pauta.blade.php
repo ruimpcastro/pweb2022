5389ebcd6cd3633ccda03d01e44ac3c918b5e262
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2022-01-24 00:57:06 +0000

    pauta aluno formatar

--- resources/views/pauta.blade.php
+++ resources/views/pauta.blade.php
@@ -4,41 +4,31 @@
     Pauta de {{ $pauta->designacao }} - {{ $disciplina->designacao }}
 @endsection
 @section('conteudo')
-    <!--TODO Daniel-->
-    {{ $pauta }}
-    {{ $disciplina }}
-
-    @foreach($pauta->resultado as $res)
-        @foreach($aluno as $a)
-            @foreach($a->resultado as $result)
-                @if ($res->id == $result->id)
-                    <p>
-                        {{$a->nome}} => {{$res->resultado}}
-                    </p>
-                @endif
-            @endforeach
-        @endforeach
-    @endforeach
-
-
     @if($pauta->dirty == 1)
+        <h3>Estado da Pauta: Publicada </h3>
+        <h4>Resultados: </h4>
 
 
-        <!--TODO Ligação para se poder visualizar a pauta publicada -->
-
-        <p>Estado da Pauta: Publicada </p>
-        <p>Resultados: </p>
-
+                @foreach($pauta->resultado as $res)
+                    @foreach($aluno as $a)
+                        @foreach($a->resultado as $result)
+                                            @if ($res->id == $result->id)
+                                                <ul style="list-style: none" class="d-flex justify-content-between align-items-center flex-row ps-0 border-bottom mb-3">
+                                                    <div class="d-flex flex-row justify-content-center align-items-center w-100">
+                                                        <div class="me-5 d-flex flex-row justify-content-between w-100">
+                                                            <li>{{$a->numero_aluno}}</li>
+                                                            <li>{{$a->nome}}</li>
+                                                            <li>{{$res->resultado}}</li>
+                                                        </div>
+                                                    </div>
+                                                </ul>
+                                            @endif
+                        @endforeach
+                    @endforeach
+                @endforeach
 
     @else
-
-        <!--TODO Adicionar ligação entre dar uploads do ficheiro e o guardar -->
-
-        <div class="input-group mb-3">
-            <input type="file" class="form-control" id="inputGroupFile01">
-            <p> Estado da Pauta: Por Publicar </p>
-        </div>
+            <h3> Estado da Pauta: Por Publicar </h3>
     @endif
-
 
 @endsection
